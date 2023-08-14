0d8c6f3088964c87b5141eac8725aa248e31f998
ekick <al.arham07@gmail.com>
2023-08-14 21:16:44 +0800

    update api

--- src/api/APIWisata.js
+++ src/api/APIWisata.js
@@ -1,17 +1,20 @@
 import axios from "axios";
+const host = 'http://localhost:8000';
 
-export const loginAPI = async (user, pass) => {
-  let respon = null, isError = false;
+let respon = null, isError = false, msgError = '';
 
+export const SaveKunjunganAPI = async ({ data, token }) => {
   const options = {
     method: 'POST',
-    url: `http://localhost:8000/login`,
-    params: { 
-      username : user,
-      password : pass,
-     }, 
+    url: `${host}/form-kunjungan`,
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    params: {
+      id_pemandu : data.id_pemandu,
+      jumlah : data.jumlah,
+    }
   };
-  
   try {
     const response = await axios.request(options);
 
@@ -19,26 +22,24 @@ export const loginAPI = async (user, pass) => {
       respon = response.data
     } else {
       isError = true
+      msgError = response.message
       throw new Error("An returning error respons from API");
     }
   } catch (error) {
+    isError = true
     console.log(error);
   }
 
-  return {respon, isError}
-
+  return {respon, isError, msgError}
 }
 
-export const registerAPI = async (user, pass) => {
-  let respon = null, isError = false;
-
+export const DataProdukAPI = async ({ token }) => {
   const options = {
-    method: 'POST',
-    url: `http://localhost:8000/register`,
-    params: { 
-      username : user,
-      password : pass,
-     }, 
+    method: 'GET',
+    url: `${host}/list-produk`,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }, 
   };
   
   try {
@@ -48,11 +49,131 @@ export const registerAPI = async (user, pass) => {
       respon = response.data
     } else {
       isError = true
+      msgError = response.message
       throw new Error("An returning error respons from API");
     }
   } catch (error) {
+    isError = true
     console.log(error);
   }
 
-  return {respon, isError}
+  return {respon, isError, msgError}
+}
+
+// export const DataSpesifikProdukAPI = async ({ data, token }) => {
+//   let respon = null, isError = false;
+
+//   const options = {
+//     method: 'GET',
+//     url: `${host}/cari-produk`,
+//     headers: {
+//       Authorization: `Bearer ${token}`
+//     },
+//     params: { 
+//       id_produk : data.id_produk,
+//      }
+//   };
+  
+//   try {
+//     const response = await axios.request(options);
+
+//     if (response.status === 200) {
+//       respon = response.data
+//     } else {
+//       isError = true
+//       throw new Error("An returning error respons from API");
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+//   return {respon, isError}
+// }
+
+export const DataSpotAPI = async ({ token }) => {
+  const options = {
+    method: 'GET',
+    url: `${host}/list-spot`,
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  }; 
+  try {
+    const response = await axios.request(options);
+
+    if (response.status === 200) {
+      respon = response.data
+    } else {
+      isError = true
+      msgError = response.message
+      throw new Error("An returning error respons from API");
+    }
+  } catch (error) {
+    isError = true
+    console.log(error);
+  }
+
+  return {respon, isError, msgError}
+}
+
+export const SavePembelianAPI = async ({ data, token }) => {
+  const options = {
+    method: 'POST',
+    url: `${host}/form-pembelian`,
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    params: { 
+      id_produk : data.id_produk,
+      jumlah : data.jumlah,
+    }
+  };
+  try {
+    const response = await axios.request(options);
+
+    if (response.status === 200) {
+      respon = response.data
+    } else {
+      isError = true
+      msgError = response.message
+      throw new Error("An returning error respons from API");
+    }
+  } catch (error) {
+    isError = true
+    console.log(error);
+  }
+
+  return {respon, isError, msgError}
+}
+
+export const SavePenyewaanAPI = async ({ data, token }) => {
+  const options = {
+    method: 'POST',
+    url: `${host}/form-penyewaan`,
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+    params: { 
+      id_produk : data.id_produk,
+      jumlah : data.jumlah,
+      waktu_sewa : data.waktu_sewa,
+      jangka_sewa : data.jangka_sewa
+    }
+  };
+  try {
+    const response = await axios.request(options);
+
+    if (response.status === 200) {
+      respon = response.data
+    } else {
+      isError = true
+      msgError = response.message
+      throw new Error("An returning error respons from API");
+    }
+  } catch (error) {
+    isError = true
+    console.log(error);
+  }
+
+  return {respon, isError, msgError}
 }
